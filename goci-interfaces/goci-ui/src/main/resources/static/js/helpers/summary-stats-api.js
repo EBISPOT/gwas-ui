6e65116ec60c74e942c387f1342222c971ee18fb
Cinzia Malangone <cinzia@ebi.ac.uk>
2018-08-15 10:03:31 +0100

    Summary stats new page

---
+++ goci-interfaces/goci-ui/src/main/resources/static/js/helpers/summary-stats-api.js
@@ -0,0 +1,45 @@
+/** This javascript requests promise-helper.js * */
+
+
+/**
+ * Make solr query.
+ * @param {String} mainEFO
+ * @param {[]String} additionalEFO
+ * @param {[]String} descendants
+ * @param {Boolean} initLoad
+ * @returns {Promise}
+ */
+function getSummaryStatsInfo(study_accession) {
+    console.log("Query Summary Stats REST API by study accession id " + study_accession);
+    return promiseGet('/gwas/summary-statistics/api/studies/'+study_accession,
+        {},'application/x-www-form-urlencoded').then(JSON.parse).then(function(data) {
+        //processVariantData(data,searchQuery);
+        console.log("Summary stats result for " + study_accession);
+        return data;
+    }).catch(function(err) {
+        console.error('Error when seaching Summary Stats data for' + study_accession + '. ' + err);
+        throw(err);
+    })
+    
+}
+
+
+/**
+ * display study table
+ * @param {Object} data - study solr docs
+ * @param {Boolean} cleanBeforeInsert
+ */
+function displayDatatableStudies(data, cleanBeforeInsert=true) {
+    //by default, we clean the table before inserting data
+    var study_ids = [];
+    if(cleanBeforeInsert){
+        $('#study-table').bootstrapTable('removeAll');
+    }
+    
+    var data_json = []
+    $.each(data, (index, study) => {
+        var tmp={};
+
+    });
+
+}
