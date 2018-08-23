ae23a4dcc672e6e8b84cfed184539bb91b30793e
Cinzia Malangone <cinzia@ebi.ac.uk>
2018-08-23 15:54:58 +0100

    clean docs

--- goci-interfaces/goci-ui/src/main/java/uk/ac/ebi/spot/goci/ui/MvcConfig.java
+++ goci-interfaces/goci-ui/src/main/java/uk/ac/ebi/spot/goci/ui/MvcConfig.java
@@ -41,8 +41,7 @@ public class MvcConfig extends WebMvcConfigurerAdapter {
         registry.addViewController("/docs/diagram-downloads").setViewName("docs-template");
         registry.addViewController("/docs/faq").setViewName("docs-template");
         // Create a static page or check if you can add just the code
-        registry.addViewController("/docs/methods").setViewName("docs-template");
-        //registry.addViewController("/docs/methods").setViewName("methods");
+        registry.addViewController("/docs/methods").setViewName("methods");
         registry.addViewController("/docs/methods/criteria").setViewName("docs-template");
         registry.addViewController("/docs/methods/curation").setViewName("docs-template");
         registry.addViewController("/docs/methods/summary-statistics").setViewName("docs-template");
