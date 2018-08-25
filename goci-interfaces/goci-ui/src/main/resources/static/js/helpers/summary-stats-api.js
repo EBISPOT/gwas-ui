01d6c05e9823aeea9478d0a3a15de851fd9b6795
Cinzia Malangone <cinzia@ebi.ac.uk>
2018-08-25 08:29:58 +0100

    Icon rel

--- goci-interfaces/goci-ui/src/main/resources/static/js/helpers/summary-stats-api.js
+++ goci-interfaces/goci-ui/src/main/resources/static/js/helpers/summary-stats-api.js
@@ -10,10 +10,9 @@
  * @returns {Promise}
  */
 function getSummaryStatsInfo(study_accession, div) {
-    console.log("Query Summary Stats REST API by study accession id " + study_accession);
+    //console.log("Query Summary Stats REST API by study accession id " + study_accession);
     return promiseGet('/gwas/summary-statistics/api/studies/'+study_accession,
         {},'application/x-www-form-urlencoded').then(JSON.parse).then(function(data) {
-        //processVariantData(data,searchQuery);
         console.log("Summary stats result for " + study_accession);
         var nextContent = div.html();
         nextContent = nextContent+ "&nbsp;&nbsp;<a href='";
@@ -25,25 +24,4 @@ function getSummaryStatsInfo(study_accession, div) {
         console.error('Error when seaching Summary Stats data for' + study_accession + '. ' + err);
         throw(err);
     })
-}
-
-
-/**
- * display study table
- * @param {Object} data - study solr docs
- * @param {Boolean} cleanBeforeInsert
- */
-function displayDatatableStudies(data, cleanBeforeInsert=true) {
-    //by default, we clean the table before inserting data
-    var study_ids = [];
-    if(cleanBeforeInsert){
-        $('#study-table').bootstrapTable('removeAll');
-    }
-    
-    var data_json = []
-    $.each(data, (index, study) => {
-        var tmp={};
-
-    });
-
 }
