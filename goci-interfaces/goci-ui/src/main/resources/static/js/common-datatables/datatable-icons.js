2267a23f3762197ee73721d2d08b200cbd9f2e6a
Cinzia Malangone <cinzia@ebi.ac.uk>
2018-09-18 08:24:49 +0100

    Icons for datatable

---
+++ goci-interfaces/goci-ui/src/main/resources/static/js/common-datatables/datatable-icons.js
@@ -0,0 +1,6 @@
+window.icons = {
+    columns: 'glyphicon glyphicon-list-alt',
+    sort: 'glyphicon glyphicon-sort',
+    export: 'glyphicon glyphicon-export icon-share',
+    refresh: 'glyphicon glyphicon-refresh icon-refresh'
+};
