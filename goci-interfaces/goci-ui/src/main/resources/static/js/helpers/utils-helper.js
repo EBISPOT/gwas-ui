70794dc45a19bacede5a7ccc8a4fa82f99a84c35
Trish Whetzel <plwhetzel@gmail.com>
2019-01-14 13:15:01 +0000

    remove unused code

--- goci-interfaces/goci-ui/src/main/resources/static/js/helpers/utils-helper.js
+++ goci-interfaces/goci-ui/src/main/resources/static/js/helpers/utils-helper.js
@@ -98,31 +98,21 @@ function displayAncestryLinksAsList(data_array) {
     var replicate_data_text = '';
 
     if (data_array) {
+        // Curation protocol: if there is one set of ancestry it will be initial/discovery
+        // study is not eligible without initial/discovery stage whereas replication stage is optional
         if (data_array.length == 1) {
             var initial;
-            // var replicate;
 
             var initial_list = $('<ul/>');
             initial_list.css('padding-left', '0px');
 
-            // var replicate_list = $('<ul/>');
-            // replicate_list.css('padding-left', '0px');
 
             if (data_array[0].startsWith('initial')) {
                 initial = data_array[0].split('|');
                 initial = initial[4] + ' ' + initial[3];
                 initial_list.append(newItem(initial))
             }
-            // else {
-            //     replicate = data_array[0].split('|');
-            //     replicate = replicate[4]+' '+replicate[3];
-            //     console.log("** One - Rep: "+replicate);
-            //     replicate_list.append(newItem(replicate))
-            //     console.log("** One A - RL: "+replicate_list);
-            // }
 
-            // initial_data_text = initial;
-            // replicate_data_text = replicate;
             initial_data_text = initial_list;
             replicate_data_text = "-";
         }
@@ -158,7 +148,6 @@ function displayAncestryLinksAsList(data_array) {
             replicate_data_text = replicate_list;
         }
     }
-    // return [initial_data_text, replicate_data_text];
     return {initial_data_text: initial_data_text, replicate_data_text: replicate_data_text};
 }
 
