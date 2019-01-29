e29984e0ab2baef5a03707f3f31618f7428eabf0
Daniel Suveges <suvi.dani@gmail.com>
2019-01-29 01:06:08 +0000

    Fixing breadcrumbs

--- goci-interfaces/goci-ui/src/main/resources/static/js/goci-documentation.js
+++ goci-interfaces/goci-ui/src/main/resources/static/js/goci-documentation.js
@@ -18,89 +18,38 @@ $(document).ready(function() {
 
 var loadDocumentation = function(pagename, content) {
 
-    // Parsing path to generate breadcrumbs:
-    var pathName = window.location.pathname;
-    pathName = pathName.replace('/gwas/',''); // Removing home link
-    pathName = pathName.replace("_", " "); // Removing underscores
-    pathName = pathName.replace('doc', 'documentation'); // Changing doc to documentation
-    if (pathName.match('download')){
-        pathName = pathName.replace('documentation', 'downloads'); // Changing downloads when required.
-    }
-    var pathComponents = pathName.split('/');
-
-    for ( var i = 0; i < pathComponents.length; i++ ){
-        // Selecting component:
-        var pathComponent = pathComponents[i];
-
-        // Generate breadcrumb title:
-        var pathTitle = pathComponent.replace('doc', 'documentation');
-
-        pathTitle = pathTitle.charAt(0).toUpperCase() + pathTitle.slice(1);
-        console.log("** Crumb component: " + pathTitle);
-        // Generate breadcrumb link:
-        var URL = gwasProperties.contextPath;
-        if ( i + 1 == pathComponents.length ){
-            $("#breadcrumb ol").append(`<li>${pathComponent}</li>`);
-        }
-        else{
-            URL = URL + "/" + pathComponent;
-            $("#breadcrumb ol").append(`<li><a href="${URL}">${pathComponent}</a></li>`);
-        }
-
-
-
-    }
-
-
-
     console.log("Attempting to load documentation...");
 
     return function(data, textStatus, jqXHR) {
-        // set breadcrumb
-        var displayName = pagename.replace(/(^| )(\w)/g, function(x) {
-            return x.toUpperCase();
-        });
-
-        displayName = displayName.replace("-", " ");
-
-        // if (displayName.toLowerCase() == "about") {
-        //     $("#help-item").removeClass("active");
-        //     $("#downloads-item").removeClass("active");
-        //     $("#about-item").addClass("active");
-        //     $("#downloads-crumb").hide();
-        //     $("#docs-crumb").show();
-        // }
-        //
-        // else
-        if (displayName.toLowerCase() == "downloads" ||
-            displayName.toLowerCase() == "file downloads" ||
-            displayName.toLowerCase() == "diagram downloads" ||
-            displayName.toLowerCase() == "summary statistics" ) {
-
-            // $("#about-item").removeClass("active");
-            $("#documentation-item").removeClass("active");
-            $("#downloads-item").addClass("active");
-            $("#docs-crumb").hide();
-            $("#downloads-crumb").show();
-
+        // Parsing path to generate breadcrumbs:
+        var pathName = window.location.pathname;
+        pathName = pathName.replace('/gwas/',''); // Removing home link
+        pathName = pathName.replace("_", " "); // Removing underscores
+        pathName = pathName.replace('doc', 'documentation'); // Changing doc to documentation
+        if (pathName.match('download')){
+            pathName = pathName.replace('documentation', 'downloads'); // Changing downloads when required.
         }
-        else {
-            // $("#about-item").removeClass("active");
-            $("#downloads-item").removeClass("active");
-            $("#documentation-item").addClass("active");
-            $("#docs-crumb").show();
-            $("#downloads-crumb").hide();
+        var pathComponents = pathName.split('/');
+
+        for ( var i = 0; i < pathComponents.length; i++ ){
+            // Selecting component:
+            var pathComponent = pathComponents[i];
+
+            // Generate breadcrumb title:
+            var pathTitle = pathComponent.replace('doc', 'documentation');
+
+            pathTitle = pathTitle.charAt(0).toUpperCase() + pathTitle.slice(1);
+            console.log("** Crumb component: " + pathTitle);
+            // Generate breadcrumb link:
+            var URL = gwasProperties.contextPath;
+            if ( i + 1 == pathComponents.length ){
+                $("#breadcrumb ol").append(`<li>${pathComponent}</li>`);
+            }
+            else{
+                URL = URL + "/" + pathComponent;
+                $("#breadcrumb ol").append(`<li><a href="${URL}">${pathComponent}</a></li>`);
+            }
         }
-        $("#current-page").text(displayName);
-
-
-
-        console.log("Updated breadcrumb (" + displayName + ")");
-        // load the data content
-        console.log("Updating " + content + "...");
-        //console.log(data);
-        content.html(data);
-
         $.getJSON(gwasProperties.contextPath+'api/search/stats')
                 .done(function(stats) {
                           setBuilds(stats);
