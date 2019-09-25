323f8f3e79ab0dc593a8714108231b16d37d26c4
jstewart <jstewart@ebi.ac.uk>
2019-09-25 13:28:41 +0100

    GOCI-2827 - fix layout for Summary Stats docs

--- goci-interfaces/goci-ui/src/main/java/uk/ac/ebi/spot/goci/ui/MvcConfig.java
+++ goci-interfaces/goci-ui/src/main/java/uk/ac/ebi/spot/goci/ui/MvcConfig.java
@@ -64,5 +64,8 @@ public class MvcConfig extends WebMvcConfigurerAdapter {
         registry.addViewController("/docs/ancestry-data").setViewName("docs-template");
         registry.addViewController("/docs/ancestry-recommendation").setViewName("docs-template");
 
+        registry.addViewController("/docs/summary-statistics").setViewName("docs-template");
+        registry.addViewController("/docs/methods/summary-statistics-format").setViewName("docs-template");
+        registry.addViewController("/docs/methods/summary-statistics-how-to").setViewName("docs-template");
     }
 }
