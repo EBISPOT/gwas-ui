7ea6b06153deda9dbd06bd8f4b4c5cb4cc3af7b5
sprintell <abayomi@sprintell.org>
2021-01-27 09:35:22 +0000

    Add reusable component for uniform api call

---
+++ goci-interfaces/goci-ui/src/main/resources/static/js/http-request-engine.js
@@ -0,0 +1,55 @@
+class HttpRequestEngine {
+
+    static fetchRequest(request) {
+        return fetch(request)
+            .then((res) => {
+                if (res.ok) {
+                    if (request.method === 'POST'){
+                        UI.launchToastNotification('Success: Data was Saved');
+                    }
+                    return res.json();
+                }
+                throw new Error('Something went wrong');
+            })
+            .then((data) => data)
+            .catch(error => {
+                UI.launchToastNotification(error);
+            });
+    }
+
+    static requestWithoutBody(uri, httpMethod) {
+        let httpHeaders = new Headers();
+        httpHeaders.append('Content-Type', 'application/json');
+        httpHeaders.append('Authorisation', 'Bearer ');
+        return  new Request(uri, {
+            method: httpMethod,
+            withCredentials: true,
+            headers: httpHeaders
+        });
+    }
+
+
+    static requestWithBody(uri, dataObj, httpMethod) {
+        let httpHeaders = new Headers();
+        httpHeaders.append('Content-Type', 'application/json');
+        httpHeaders.append('Accept', 'application/json');
+        return  new Request(uri, {
+            method: httpMethod,
+            withCredentials: true,
+            headers: httpHeaders,
+            body: JSON.stringify(dataObj)
+        });
+    }
+
+    static fileUploadRequest(uri, dataObj, httpMethod) {
+        let httpHeaders = new Headers();
+        httpHeaders.append('Accept', 'application/json');
+        return new Request(uri, {
+            method: httpMethod,
+            withCredentials: true,
+            headers: httpHeaders,
+            body: dataObj
+        });
+    }
+
+}
