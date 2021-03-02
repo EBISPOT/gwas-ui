3ecb2d4c91d43666581d4ff0627afc488c7af7b2
sprintell <abayomi@sprintell.org>
2021-03-02 11:52:51 +0000

    gwas-ui#208 - update spring boot from v1.4 to v2.4.2

---
+++ src/test/java/uk/ac/ebi/spot/goci/GwasUiApplicationTests.java
@@ -0,0 +1,13 @@
+package uk.ac.ebi.spot.goci;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GwasUiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
