80ab404f044aab1acb6c4100408193ac3866d0b1
sprintell <abayomi@sprintell.org>
2022-10-21 14:56:13 +0100

    Add Data Model and dependencies to process solr data on the server side

---
+++ goci-interfaces/goci-ui/src/main/java/uk/ac/ebi/spot/goci/model/solr/Params.java
@@ -0,0 +1,40 @@
+package uk.ac.ebi.spot.goci.model.solr;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonPropertyOrder({
+        "q",
+        "fl",
+        "start",
+        "fq",
+        "rows",
+        "wt"
+})
+public class Params {
+
+    @JsonProperty("q")
+    private String q;
+    @JsonProperty("fl")
+    private String fl;
+    @JsonProperty("start")
+    private String start;
+    @JsonProperty("fq")
+    private String fq;
+    @JsonProperty("rows")
+    private String rows;
+    @JsonProperty("wt")
+    private String wt;
+
+}
