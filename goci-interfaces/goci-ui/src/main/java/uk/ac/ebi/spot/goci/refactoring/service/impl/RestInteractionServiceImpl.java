9752cf243e8c8cc551f9bbda3ed410827352ce80
ala <63405719+ala-ebi@users.noreply.github.com>
2023-08-11 15:33:30 +0100

    4.0.3:  Release done

--- goci-interfaces/goci-ui/src/main/java/uk/ac/ebi/spot/goci/refactoring/service/impl/RestInteractionServiceImpl.java
+++ goci-interfaces/goci-ui/src/main/java/uk/ac/ebi/spot/goci/refactoring/service/impl/RestInteractionServiceImpl.java
@@ -118,9 +118,15 @@ public class RestInteractionServiceImpl implements RestInteractionService {
         HttpEntity<MultiValueMap<String, String>> httpEntity = new HttpEntity<>(paramsMap, headers);
         ResponseEntity<SolrData> responseEntity = null;
         try {
+            StringBuilder stringBuilder = new StringBuilder();
+            for(String str : paramsMap.keySet()){
+                stringBuilder.append(str).append("=").append(paramsMap.getFirst(str)).append("&");
+            }
+            System.out.println("Copy this: " + stringBuilder);
             responseEntity = restTemplate.exchange(uri, HttpMethod.POST, httpEntity, new ParameterizedTypeReference
                 <SolrData>() {
-        });
+            });
+
         }catch(Exception ex){
             log.error("Exception in Rest API call"+ex.getMessage(),ex);
         }
